9c9a98e4a3cd5d2e18ed74c8d9e2035764ededd3
wly <1215889492@qq.com>
2021-05-30 10:12:35 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,30 @@
+'''print("nihao")#字符串
+print(111)
+print(True)
+print({})
+
+print(444.11)
+print("哈哈哈",2222)
+print(111,{})
+print("haha"+"xoxo")
+print("ww"*100)
+print({}+[])
+print(((1+2)*10-12)/2)
+print(2>3)
+name="zhangsan"
+print(name)
+'''
+# a = input("请输入：")
+# print("input获取的值:",a)
+# print(type("haha"))
+# print(type(2.33))
+# print(type({}))
+# a=float(input("请输入："))
+# b=float(input("请输入："))
+# print("input获取的值:",a+b)
+# a="KKHOJHI ojkojk"
+# print(len(a))
+"""练习：通过代码获取2段不同的代码的内容，并计算他们长度的值"""
+a = input("请输入:")
+b = input("请输入:")
+print("字数",len(a)+len(b))
